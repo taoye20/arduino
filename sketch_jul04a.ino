44193795688703f5a46e0c00e832bc55f099015b
taoye20 <52461257+taoye20@users.noreply.github.com>
2019-07-04 15:07:12 +0800

    Add files via upload

--- sketch_jul04a.ino
+++ sketch_jul04a.ino
@@ -1,72 +1,29 @@
 void setup()
 {
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-    pinMode(9,OUTPUT);
-    pinMode(10,OUTPUT);
-  
-   Serial.begin(9600);
-    pinMode(8,OUTPUT);
-    pinMode(7,OUTPUT);;
-     
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
 }
-
+ int income;
 void loop()
 {
-  int income=0;
+   digitalWrite(6,LOW);
+  delay(10);
+ 
   if(Serial.available()>0)
   {
     income=Serial.read();
-    switch(income)
-    {
-      case 'f': forword();break;
-      case 'b': backword();break;
-      case 'r': right();break;
-      case 'l': left();break;
-      case 's': stop();break;
-      
-    }
   }
+  income=income-'0';
+   digitalWrite(2,income&0x1?HIGH:LOW);
+  digitalWrite(3,income&(0x1<<1)?HIGH:LOW);
+  digitalWrite(4,income&(0x1<<2)?HIGH:LOW);
+  digitalWrite(5,income&(0x1<<3)?HIGH:LOW);
+  delay(1000);
+  
+  
 }
-
-void forword()
-{
-  digitalWrite(5,HIGH);
-  digitalWrite(6,LOW);
-  digitalWrite(10,HIGH);
-  digitalWrite(9,LOW);
-}
-
-void backword()
-{
-  digitalWrite(6,HIGH);
-  digitalWrite(5,LOW);
-  digitalWrite(10,HIGH);
-  digitalWrite(9,LOW);
-}
-
-void right()
-{
-  digitalWrite(6,HIGH);
-  digitalWrite(5,LOW);
-  digitalWrite(9,HIGH);
-  digitalWrite(10,LOW);
-  digitalWrite(8,HIGH);
-}
-
-void left()
-{
-  digitalWrite(5,HIGH);
-  digitalWrite(6,LOW);
-  digitalWrite(9,HIGH);
-  digitalWrite(10,LOW);
-  digitalWrite(9,HIGH);
-}
-
-void stop()
-{
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(9,LOW);
-  digitalWrite(10,LOW);
-}
+  
